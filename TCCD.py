a118c5d9646c41ce676d2cb18e73526279a88143
orie1876 <65364777+orie1876@users.noreply.github.com>
2020-06-29 17:02:17 +0100

    Outputs histograms and has a MaxQ function

--- TCCD.py
+++ TCCD.py
@@ -7,8 +7,9 @@ Created on Mon Jun 29 08:32:47 2020
 """
 
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 import csv
+
 
 
 # The thresholds and filenames etc. need to be placed below
@@ -17,8 +18,8 @@ path = r"/Users/Mathew/Dropbox (Cambridge University)/Ed Code/TCCD Analysis/File
 
 file_stem="DNA"         # This is the filename before the underscore. 
 number_of_files=2       # Number of files in the folder (could make this automatic in the future).
-channelA_thresh=10      # Threshold for Channel A (Green).
-channelB_thresh=10      # Threshold for Channel B (Red).
+channelA_thresh=5      # Threshold for Channel A (Green).
+channelB_thresh=12      # Threshold for Channel B (Red).
 channelA_AF=1.16        # Autofluorescence
 channelB_AF=1.13
 xtalk=0.237             # Cross-talk from A to B
@@ -82,4 +83,67 @@ var_chance_events=float(len(channelA_chance))
 Q=float((var_real_events-var_chance_events)/(var_A_events+var_B_events-(var_real_events-var_chance_events)))
 
 print ('There were %s A events, %s B events, %s coincidence events, and %s chance events. Q = %f.')%(var_A_events,var_B_events,var_real_events,var_chance_events,Q)
+
+
+# Now want histograms etc. 
+
+ln_events=np.log(channelB_events/channelA_events)
+ln_chance=np.log(channelB_chance/channelA_chance)
+
+textstr='Q = %.3f'%Q
+
+plt.rcParams["font.family"] = "Arial"
+plt.rcParams["font.size"] = "12"
+plt.figure(figsize=(8, 6))
+plt.hist(ln_events, bins = 60,range=[-3,3], rwidth=0.9,ec='black',color='#ff0000',alpha=0.8,label="Real Events")
+plt.hist(ln_chance, bins = 60,range=[-3,3], rwidth=0.9,ec='black',color='#cccccc',alpha=0.5,label="Chance Events")
+plt.text(0.05,0.90, textstr,transform=plt.gca().transAxes)
+plt.legend(loc='upper right')         
+plt.xlabel('Z=$ln(I_{B}/I_{A}$)')
+plt.ylabel('Number of events')
+plt.savefig(path+'/'+'lnz.pdf')
+plt.show()
+
+
+
+def maxQ():
+    q_vals = np.zeros(shape=(20,20))
+
+    for A in range(20):
+        for B in range(20):
+            channelA_only_events=channelA_arr[(channelA_arr>A)]                       # Total A events
+            channelB_only_events=channelB_arr[(channelB_arr>B)]                       # Total B events
+            channelA_events=channelA_arr[np.logical_and(channelA_arr>A, channelB_arr>B)]  # A coincident events             
+            
+            
+            # Now need to account for chance events:
+            
+            channelB_shuffle=channelB_arr.copy()
+            np.random.shuffle(channelB_shuffle)
+            
+            channelA_chance=channelA_arr[np.logical_and(channelA_arr>A, channelB_shuffle>B)]    # These are the chance events    
+            
+            # Now need to calculate Q value:
+            
+            var_real_events=float(len(channelA_events))
+            var_A_events=float(len(channelA_only_events))
+            var_B_events=float(len(channelB_only_events))
+            var_chance_events=float(len(channelA_chance))
+            Q=float((var_real_events-var_chance_events)/(var_A_events+var_B_events-(var_real_events-var_chance_events)))
+    
+            q_vals[A][B]=Q
+            
+
+    maximum_Q=np.amax(q_vals)
+    result=np.where(q_vals == np.amax(q_vals))
+    ThresholdA,ThresholdB=result
+    
+    print 'The maximum value of Q is %.3f, with a threshold of %s in channel A, and %s in channel B.'%(maximum_Q,str(ThresholdA),ThresholdB)
+    
+    
+    contourplot = plt.contourf(q_vals,20,origin='lower')
+    cbar = plt.colorbar(contourplot)
+    plt.xlabel("Channel B Threshold")
+    plt.ylabel("Channel A Threshold")
+    cbar.ax.set_ylabel('Q')
 
